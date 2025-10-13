37e0780b66706864c54e90dbeda4166a4168c399
sigmajove <sigmajove@gmail.com>
2025-10-13 01:02:55 -0700

    Serious progress. Implemented a form a guessing Fixed bug in Card 25

--- turing-machine.cc
+++ turing-machine.cc
@@ -185,7 +185,13 @@ const std::array<Verifier, 48> verifiers = {
 
      // Card 25
      {[](int i, int j, int k) { return abs(i - j) != 1 && abs(j - k) != 1; },
-      [](int i, int j, int k) { return (abs(i - j) == 1) ^ (abs(j - k) == 1); },
+      [](int i, int j, int k) {
+        // Note: 121 return true, but 123 and 321 return false.
+        if (i + 1 == j && j + 1 == k || i - 1 == j && j - 1 == k) {
+          return false;
+        }
+        return (abs(i - j) == 1) || (abs(j - k) == 1);
+      },
       [](int i, int j, int k) {
         return i + 1 == j && j + 1 == k || i - 1 == j && j - 1 == k;
       }},
@@ -378,15 +384,42 @@ std::optional<Triple> solve(const Selected& selected, bool trace = false) {
   return result;
 }
 
+std::vector<Triple> Matching(const Verifier& v, std::size_t which) {
+  if (which >= v.size()) {
+    throw std::runtime_error(
+        std::format("which {} out of bounds {}", which, v.size()));
+  }
+  const Criterion c = v[which];
+  std::vector<Triple> result;
+  for (int i = 1; i <= 5; ++i) {
+    for (int j = 1; j <= 5; ++j) {
+      for (int k = 1; k <= 5; ++k) {
+        if (c(i, j, k)) {
+          const Triple t{i, j, k};
+          result.push_back(t);
+        }
+      }
+    }
+  }
+  return result;
+}
+
 class Analyzer {
  public:
   explicit Analyzer(const std::vector<std::size_t>& cards);
   std::size_t size() const { return selected_.size(); }
+  const Verifier& get_verifier(std::size_t which) const {
+    return cards_[which];
+  }
 
   void Restrict(const Triple& code, std::size_t card, bool success);
 
   void GenerateCombinations(std::size_t start);
   std::map<Triple, std::vector<std::set<std::size_t>>> result_;
+
+  // For each card, a count of times each number shows up
+  // in selection.
+  std::vector<std::map<std::size_t, std::size_t>> distrib_;
 
  private:
   std::vector<Verifier> cards_;
@@ -394,7 +427,7 @@ class Analyzer {
 };
 
 Analyzer::Analyzer(const std::vector<std::size_t>& cards)
-    : selected_(cards.size()) {
+    : selected_(cards.size()), distrib_(cards.size()) {
   for (std::size_t i : cards) {
     // Convert 1-based card numbers to 0-based subscripts.
     const Verifier v = verifiers[i - 1];
@@ -439,6 +472,10 @@ void Analyzer::GenerateCombinations(std::size_t start) {
             result_.try_emplace(*answer, cards_.size()).first->second;
         for (std::size_t i = 0; i < xxx.size(); ++i) {
           xxx[i].insert(selected_[i]);
+        }
+
+        for (std::size_t card = 0; card < selected_.size(); ++card) {
+          ++(distrib_[card].try_emplace(selected_[card], 0).first->second);
         }
       }
     }
@@ -577,6 +614,85 @@ int main(int argc, char* argv[]) {
       }
       std::cout << "]\n";
     }
+
+    switch (analyzer.result_.size()) {
+      case 0:
+        std::cout << "No answer!\n";
+        return 0;
+      case 1:
+        std::cout << "Time to guess\n";
+        return 0;
+    }
+
+#if 0
+    std::cout << "\nDistribution\n";
+    for (const auto& m : analyzer.distrib_) {
+      std::cout << "===============\n";
+      for (auto [k, v] : m) {
+        std::cout << k << " -> " << v << "\n";
+      }
+    }
+    std::cout << "===============\n";
+#endif
+
+    std::map<Triple, std::set<std::size_t>> guesses;
+
+    for (std::size_t card = 0; card < analyzer.distrib_.size(); ++card) {
+      const auto& m = analyzer.distrib_[card];
+      if (m.size() <= 1) {
+        continue;
+      }
+#if 0
+      std::cout << "Processes distribution for card " << card << "\n";
+#endif
+      for (const auto& [which, count] : m) {
+#if 0
+        std::cout << which << " " << count << "\n";
+#endif
+        auto xxx = Matching(analyzer.get_verifier(card), which);
+        for (const Triple& t : xxx) {
+          guesses.try_emplace(t).first->second.insert(card);
+        }
+      }
+    }
+
+    // Find the maximal guess set.
+    std::size_t guess_size = 0;
+    for (const auto& [t, s] : guesses) {
+      guess_size = std::max(guess_size, s.size());
+    }
+    // At most three guesses.
+    guess_size = std::min(guess_size, static_cast<std::size_t>(3));
+
+    // Find a guess at that big.
+    std::cout << "\nSuggested guesses\n";
+    for (const auto& [t, s] : guesses) {
+      if (s.size() >= guess_size) {
+        std::cout << std::format("{}{}{}: ", t[0], t[1], t[2]);
+
+        std::size_t used = 0;
+        for (auto q : s) {
+          std::cout << static_cast<char>('a' + q);
+          if (++used >= guess_size) {
+            break;
+          }
+        }
+        std::cout << std::format(" (size: {})", s.size());
+        std::cout << "\n";
+      }
+    }
+
+#if 0
+    std::cout << "\nAll of them\n";
+    for (const auto& [t, s] : guesses) {
+      std::cout << std::format("{}{}{}: ", t[0], t[1], t[2]);
+      for (auto i : s) {
+        std::cout << i;
+      }
+      std::cout << "\n";
+    }
+#endif
+
   } catch (const std::runtime_error& e) {
     std::cerr << e.what() << "\n";
     return 1;
