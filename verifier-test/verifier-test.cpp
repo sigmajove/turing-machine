52f8fcfe94f754734cef0d5557a12b2d40c22eb6
sigmajove <sigmajove@gmail.com>
2025-10-14 00:40:11 -0700

    I think I have the code right for testing all the verifiers.

--- verifier-test/verifier-test.cpp
+++ verifier-test/verifier-test.cpp
@@ -23,53 +23,57 @@ std::vector<Triple> RandTriples(std::mt19937& rand) {
 }
 
 int moon() {
-  std::cout << "Good:";
   for (std::size_t card = 0; card < verifiers.size(); ++card) {
+    bool notable = false;
     const Verifier& v = verifiers[card];
-    bool all_good = true;
     for (int i = 1; i <= 5; ++i) {
       for (int j = 1; j <= 5; ++j) {
         for (int k = 1; k <= 5; ++k) {
-          std::size_t success = 0;
+          bool all_miss = true;
           for (Criterion c : v) {
-            if (c(i, j, k)) {
-              success += 1;
-            }
+            all_miss = all_miss & !c(i, j, k);
           }
-          if (success != 1) {
-            all_good = false;
+          if (all_miss) {
+            notable = true;
           }
         }
       }
     }
-    if (all_good) {
-      std::cout << " " << card + 1;
+    if (notable) {
+      std::cout << "Card " << card + 1 << "\n";
     }
   }
-  std::cout << "\n";
   return 0;
 }
 
 int main() {
   std::mt19937 rand(12345);
-  for (std::size_t card = 1; card <= 25; ++card) {
-    if (card == 14 || card == 15) {
+  for (std::size_t card = 14; card <= 48; ++card) {
+    if (16 <= card && card <= 25) {
       continue;
     }
     const Verifier& v = verifiers[card - 1];
-    std::cout << std::format("\n{{{},{{\n", card);
-    for (std::size_t i = 0; i < v.size(); ++i) {
-      std::cout << "{";
-      std::size_t count = 0;
-      for (const Triple& t : RandTriples(rand)) {
-        if (v[i](t[0], t[1], t[2])) {
-          if (count != 0) {
-            std::cout << ", ";
-          }
-          std::cout << std::format("{{{},{},{}}}", t[0], t[1], t[2]);
-          if (++count >= 5) break;
+    std::vector<std::array<std::vector<Triple>, 2>> bucket(v.size());
+    for (const Triple& t : RandTriples(rand)) {
+      for (std::size_t i = 0; i < v.size(); ++i) {
+        std::vector<Triple>& dest = bucket[i][!(v[i](t[0], t[1], t[2]))];
+        if (dest.size() < 5) {
+          dest.push_back(t);
         }
       }
+    }
+    std::cout << std::format("\n{{{},{{\n", card);
+    for (const auto& b : bucket) {
+      std::cout << "{";
+      for (const auto& c : b) {
+        std::cout << "\n{";
+        for (const Triple& t : c) {
+          std::cout << std::format("{{{},{},{}}}, ", t[0], t[1], t[2]);
+        }
+        std::cout << "}";
+      }
+#if 0
+#endif
       std::cout << "},\n";
     }
     std::cout << "}},\n";
