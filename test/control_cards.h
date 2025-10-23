10cdd2e857a646477a8b311ab93a4e55de3bab02
sigmajove <sigmajove@gmail.com>
2025-10-22 20:30:17 -0700

    Added constants for control cards

---
+++ test/control_cards.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <array>
+
+// These are the 95 cards that reveal the answers to quereys.
+// Each card has 131 check or x boxes, 
+extern std::array<std::array<bool, 131>, 95> control_cards;
