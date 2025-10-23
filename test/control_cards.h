e6693e13b905965d577c33f619bdd9df71c51bb0
sigmajove <sigmajove@gmail.com>
2025-10-22 20:32:25 -0700

    Fixed comment

--- test/control_cards.h
+++ test/control_cards.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <array>
 
-// These are the 95 cards that reveal the answers to quereys.
-// Each card has 131 check or x boxes, 
+// These are the 95 cards that reveal the answers to queries.
+// Each card has 131 check or x boxes. How they are addressed is TBD.
 extern std::array<std::array<bool, 131>, 95> control_cards;
